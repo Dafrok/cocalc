ed7b5ee2db08eb283d54ae1915b9ff9a9932ab6a
Dafrok <o.o@mug.dog>
2016-04-06 22:13:55 +0800

    Remove useless src

--- gulpfile.js
+++ gulpfile.js
@@ -5,34 +5,21 @@ gulp.task('reload', [], () => {
     browserSync.reload()
 })
 
-gulp.task('browser-sync', ['base', 'components', 'index'], () => {
+gulp.task('browser-sync', ['base'], () => {
     browserSync.init({
         server: {
             baseDir: ''
         }
     })
-    gulp.watch('./src/components/**', ['components'])
-    gulp.watch('./src/index.html', ['index'])
-    gulp.watch('./static/**/*.*', ['reload'])
+    gulp.watch('./components/**/*.*', ['reload'])
+    gulp.watch('./vendor/**/*.*', ['reload'])
     gulp.watch('./index.html', ['reload'])
-})
-
-gulp.task('index', [], () => {
-    gulp
-        .src('./src/index.html')
-        .pipe(gulp.dest('./'))
-})
-
-gulp.task('components', [], () => {
-    gulp
-        .src('./src/components/**')
-        .pipe(gulp.dest('./static/components'))
 })
 
 gulp.task('base', [], () => {
     gulp
         .src('./bower_components/**')
-        .pipe(gulp.dest('./static/vendor'))
+        .pipe(gulp.dest('./vendor'))
 })
 
 gulp.task('default', ['browser-sync'], () => {
