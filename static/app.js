abb62a0f5a66e81f79a55c06272338143d764571
Dafrok <o.o@mug.dog>
2016-04-21 18:40:29 +0800

    page name

--- static/app.js
+++ static/app.js
@@ -5,6 +5,16 @@
     app.scrollPageToTop = function() {
         // app.$.headerPanelMain.scrollToTop(true);
     }
+
+    app.pageName = {
+        recents: 'RECENTS',
+        members: 'MEMBERS',
+        wars: 'WARS',
+        power: 'POWER',
+        calculagraph: 'CALCULAGRAPH',
+        charts: 'CHARTS',
+        about: 'ABOUT'
+    }
     window.addEventListener('WebComponentsReady', function() {
         // imports are loaded and elements have been registered
         console.log('ready')
